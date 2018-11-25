db4935767596a4bbf5d6d00fc66506a1c7a73111
Tiffanatic <madsrynord@live.dk>
2018-11-25 23:46:14 +0100

    first commit

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace DiscordBot
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            args = new string[0];
+
+            if(args.Any() && args[0] == "--version")
+            {
+                Console.WriteLine("0.0.1");
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
